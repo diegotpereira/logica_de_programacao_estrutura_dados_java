627e02dda9d7292638e6cbf9bd6f202ce0c9da4b
diegotpereira <diegoteixeirapereira@hotmail.com>
2021-12-23 11:45:42 -0300

    criação parcial do algoritmo de ordenação completo.

---
+++ ordenacaoCompleto/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Programa completo para ordenação.
+        int opcao;
+        int qte;
+
+        String num;
+        String saida = "| ";
+
+        try {
+            
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+    }
+}
