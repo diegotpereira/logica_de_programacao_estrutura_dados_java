f7440241778e2b8f463a681f47bbaf47590c0539
diegotpereira <diegoteixeirapereira@hotmail.com>
2021-12-23 10:04:08 -0300

    criação do app java com exemplo de vetor.

---
+++ estruturaDadosEstaticosVetor5/src/App.java
@@ -0,0 +1,38 @@
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // Este exemplo efetua a ordenação dos elementos 
+        // considerados no exemplo anterior, exibindo o maior 
+        // e o menor deles.
+        final int diasSemana = 7;
+        float temperatura[] = new float[diasSemana];
+        temperatura[0] = 19.0f;
+        temperatura[1] = 23.0f;
+        temperatura[2] = 21.0f;
+        temperatura[3] = 25.0f;
+        temperatura[4] = 22.0f;
+        temperatura[5] = 20.0f;
+        temperatura[6] = 24.0f;
+        float x;
+
+        for(int i = 0; i < diasSemana; i ++) {
+            for(int j = i + 1; j < diasSemana; j++) {
+
+                if(temperatura[j] < temperatura[i]) {
+
+                    x = temperatura[i];
+                    temperatura[i] = temperatura[j];
+                    temperatura[j] = x;
+                }
+            }
+        }
+
+        JOptionPane.showMessageDialog(
+            null, "Minima da semana = " + temperatura[0]);
+
+        JOptionPane.showMessageDialog(
+            null, "Máxima da semana = " + temperatura[6]);
+    }
+}
