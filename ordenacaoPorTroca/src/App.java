2ba418eadf63f6b26c91736b8fa13d634c5a4379
diegotpereira <diegoteixeirapereira@hotmail.com>
2021-12-23 11:37:35 -0300

    criação do app java com exemplo de ordenação por troca.

---
+++ ordenacaoPorTroca/src/App.java
@@ -0,0 +1,26 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Algoritmo de ordenação por trocas – método da bolha.
+        int numeros[] = {23, 4, 33, 45, 19, 12, 28, 40};
+        int x;
+
+        for(int i = 1; i < numeros.length; i++) {
+
+            for(int j = numeros.length - 1; j >= i; j--) {
+
+                if (numeros[j-1] > numeros[j]) {
+                    
+                    x = numeros[j-1];
+                    numeros[j-1] = numeros[j];
+                    numeros[j] = x;
+                }
+            }
+
+            System.out.printf("\n%s%2d", "Iteração", i);
+
+            for(int k =0; k < numeros.length; k++) {
+                System.out.printf("%5d", numeros[k]);
+            }
+        }
+    }
+}
